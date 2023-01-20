a6a96b41f81dbbf3e583d16e5390ce66d1a056b3
Mikael Svensson <113359307+mikeCre8@users.noreply.github.com>
2023-01-20 11:24:36 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/workshop_datajpa/WorkshopDataJpaApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.workshop_datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopDataJpaApplication {
+    
+    public static void main( String[] args ) {
+        SpringApplication.run(WorkshopDataJpaApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/se/lexicon/workshop_datajpa/WorkshopDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.workshop_datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopDataJpaApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
